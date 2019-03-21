f45be941a0d076f6812f9a911a04cd747588abd4
Mohammed Thabit <mohammedthabet100@gmail.com>
2019-03-21 14:22:40 +0100

    Initial commit.

---
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.mohammedthabet.realmadrid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    public void launchWebActivity(View view) {
+
+        String url;
+        //Send url to WebActivity
+        Intent intent = new Intent(this, WebActivity.class);
+        //Put the url in the intent
+        int viewId = view.getId();
+        switch(viewId){
+            case R.id.image_Show_History: url = "https://www.realmadrid.com/en/about-real-madrid/history/football";
+                    break;
+
+            case R.id.image_Show_News: url="https://www.newsnow.co.uk/h/Sport/Football/La+Liga/Real+Madrid/Transfer+news";
+                break;
+
+            case R.id.image_Show_GamesSchedule: url="https://www.google.de/search?q=real+madrid+games+schedule&oq=real+madrid+games+schedule&aqs=chrome..69i57j0l2.9638j1j4&sourceid=chrome&ie=UTF-8#sie=t;/m/06l22;2;/m/09gqx;mt;fp;1;;";
+                break;
+
+            case R.id.image_Show_Website: url="https://www.realmadrid.com/en";
+                break;
+
+                default: url="https://www.google.com/";
+        }
+        //Bundle the urk in an intent extra
+        intent.putExtra("url", url);
+        startActivity(intent);
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/WebActivity.java
@@ -0,0 +1,37 @@
+package com.example.mohammedthabet.realmadrid;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class WebActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webview = findViewById(R.id.web_view);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        Intent intent = getIntent();
+        String link = intent.getStringExtra("url");
+        //Get url from Intent
+        webview.loadUrl(link);
+
+
+
+    }
+    
+
+}
