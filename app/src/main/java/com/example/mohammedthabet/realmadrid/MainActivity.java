e3f7580ebf37f30cdb8d7b101c91e26b827c8880
Tony Albanese <tony.albanese2000@gmail.com>
2019-03-26 17:57:53 +0100

    Added onClickListener to the ImageView for the games schedule.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/MainActivity.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.mohammedthabet.realmadrid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView schedulesImage; //Declare a reference to the ImageView for the schedules.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        schedulesImage = findViewById(R.id.image_show_schedule); //Use the genie to connect the reference to the real object.
+        schedulesImage.setOnClickListener(new View.OnClickListener() {//Set an onClick listener for the ImageView.
+            @Override
+            public void onClick(View v) {
+                //TODO: Launch the GamesScheduleActivity! You know how to do this.
+            }
+        });
     }
 
 
