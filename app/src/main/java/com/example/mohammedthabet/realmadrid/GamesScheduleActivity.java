07ee1348e7c360dd6b78aaa597f8117b30f71000
Tony Albanese <tony.albanese2000@gmail.com>
2019-03-26 17:36:19 +0100

    Refactored GamesSchedule to GamesScheduleActivity.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/GamesScheduleActivity.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/GamesScheduleActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class GamesSchedule {
+public class GamesScheduleActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
