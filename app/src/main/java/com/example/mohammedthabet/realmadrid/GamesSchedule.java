5aa995a17785ca5891829f56fd7cd483179f5ebf
Mohammed Thabit <mohammedthabet100@gmail.com>
2019-03-26 15:03:15 +0100

    Initial commit.

---
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/GamesSchedule.java
@@ -0,0 +1,27 @@
+package com.example.mohammedthabet.realmadrid;
+
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class GamesSchedule {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+
+        //Set the onClick listener for the button.
+        Button searchButton = findViewById(R.id.image_Show_GamesSchedule);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //This is the code that gets run when the button is clicked.
+                startLoader();
+            }
+        });
+
+    }
+
+}
