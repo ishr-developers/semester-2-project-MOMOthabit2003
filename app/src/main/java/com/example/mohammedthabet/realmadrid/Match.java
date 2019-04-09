411e2730a308da6254cd82363fbad2adf0df71f4
tony-albanese <tony.albanese2000@gmail.com>
2019-04-09 15:00:55 +0200

    Processed the json response and setup the Match class.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/Match.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/Match.java
@@ -1,4 +1,48 @@
 package com.example.mohammedthabet.realmadrid;
 
 public class Match {
+
+    String awayTeamName;
+    String homeTeamName;
+    String date;
+    String competitionName;
+
+    public Match(String competitionName, String homeTeamName, String awayTeamName, String date) {
+        this.awayTeamName = awayTeamName;
+        this.homeTeamName = homeTeamName;
+        this.date = date;
+        this.competitionName = competitionName;
+    }
+
+    public String getAwayTeamName() {
+        return awayTeamName;
+    }
+
+    public void setAwayTeamName(String awayTeamName) {
+        this.awayTeamName = awayTeamName;
+    }
+
+    public String getHomeTeamName() {
+        return homeTeamName;
+    }
+
+    public void setHomeTeamName(String homeTeamName) {
+        this.homeTeamName = homeTeamName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCompetitionName() {
+        return competitionName;
+    }
+
+    public void setCompetitionName(String competitionName) {
+        this.competitionName = competitionName;
+    }
 }
