d9b98d88fac34c1b9ef952c6ca2f81adba8a6fe6
tony-albanese <tony.albanese2000@gmail.com>
2019-04-11 10:47:58 +0200

    Added RecyclerView inner class and constructor.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
@@ -1,6 +1,39 @@
 package com.example.mohammedthabet.realmadrid;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 public class MatchesRecyclerAdapter extends RecyclerView.Adapter {
+    ArrayList<Match> list = new ArrayList<Match>();
+
+    public MatchesRecyclerAdapter(ArrayList<Match> listFromActivty) {
+        list = listFromActivty;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MatchesViewHolder extends RecyclerView.ViewHolder {
+
+        public MatchesViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
