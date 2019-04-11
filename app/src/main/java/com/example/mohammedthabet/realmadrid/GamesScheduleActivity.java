355a9225db321c928a58f28650b9e18f9a78e1e7
Mohammed Thabit <mohammedthabet100@gmail.com>
2019-04-11 14:15:52 +0200

    Deleted a TODO, Changed the layout file name to activity_schedule_item_layout, Created a file for the recycler view, Inserted a recyclerview to the new file.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/GamesScheduleActivity.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/GamesScheduleActivity.java
@@ -25,7 +25,7 @@ public class GamesScheduleActivity extends AppCompatActivity implements LoaderMa
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule_layout);
+        setContentView(R.layout.schedule_layout);
 
         //These functions will be called whenever the Activity starts.
 
