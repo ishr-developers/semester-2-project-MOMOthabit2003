408a7289eda9a801fa7ab0d7ac52fc0726faca6c
Tony Albanese <tony.albanese2000@gmail.com>
2019-04-30 16:34:58 +0200

    Completed more of the recycler view.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
@@ -2,16 +2,22 @@ package com.example.mohammedthabet.realmadrid;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class MatchesRecyclerAdapter extends RecyclerView.Adapter<MatchesRecyclerAdapter.MatchesViewHolder> {
+
+    ArrayList<Match> list = new ArrayList<Match>();
 
 
     @NonNull
     @Override
     public MatchesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+       View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_schedule_item_layout, viewGroup, false);
+        return new MatchesViewHolder(view);
     }
 
     @Override
