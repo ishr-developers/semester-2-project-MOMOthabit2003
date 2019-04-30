4dd96b79ed83e7162628d1f6c41998484a60b37f
Tony Albanese <tony.albanese2000@gmail.com>
2019-04-30 16:31:35 +0200

    Added proper imports and constructor.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
@@ -2,47 +2,32 @@ package com.example.mohammedthabet.realmadrid;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import org.w3c.dom.Text;
+public class MatchesRecyclerAdapter extends RecyclerView.Adapter<MatchesRecyclerAdapter.MatchesViewHolder> {
 
-import java.util.ArrayList;
-
-public class MatchesRecyclerAdapter extends RecyclerView.Adapter {
-    ArrayList<Match> list = new ArrayList<Match>();
-
-    public MatchesRecyclerAdapter(ArrayList<Match> listFromActivty) {
-        list = listFromActivty;
-    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View listItemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_schedule_item_layout, viewGroup, false);
-        return new MatchesViewHolder(listItemView);
+    public MatchesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull MatchesViewHolder matchesViewHolder, int i) {
 
     }
 
     @Override
     public int getItemCount() {
-
-        return list.size();
+        return 0;
     }
 
-    public class MatchesViewHolder extends RecyclerView.ViewHolder {
-
-        public TextView homeTeamTextView;
-        public TextView awayTeamTextView;
-
+    class MatchesViewHolder extends RecyclerView.ViewHolder {
         public MatchesViewHolder(@NonNull View itemView) {
             super(itemView);
         }
     }
+
 }
