74245a7f882759f75b848d21d631138fbec512ff
tony-albanese <tony.albanese2000@gmail.com>
2019-05-02 13:57:07 +0200

    Implemented recycler adapter.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/MatchesRecyclerAdapter.java
@@ -21,12 +21,19 @@ public class MatchesRecyclerAdapter extends RecyclerView.Adapter<MatchesRecycler
     @NonNull
     @Override
     public MatchesViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-       View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_schedule_item_layout, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.activity_schedule_item_layout, viewGroup, false);
         return new MatchesViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MatchesViewHolder matchesViewHolder, int i) {
+        Match currentMatch = list.get(i);
+
+
+        matchesViewHolder.competitionTextView.setText(currentMatch.competitionName);
+        matchesViewHolder.venueTextView.setText(currentMatch.venueName);
+        matchesViewHolder.awayTeamTextView.setText(currentMatch.awayTeamName);
+        matchesViewHolder.homeTeamTextView.setText(currentMatch.homeTeamName);
 
 
     }
@@ -39,12 +46,18 @@ public class MatchesRecyclerAdapter extends RecyclerView.Adapter<MatchesRecycler
     class MatchesViewHolder extends RecyclerView.ViewHolder {
         TextView homeTeamTextView;
         TextView awayTeamTextView;
+        TextView venueTextView;
+        TextView competitionTextView;
+
 
         public MatchesViewHolder(@NonNull View itemView) {
             super(itemView);
             homeTeamTextView = itemView.findViewById(R.id.tv_home_team_name);
             awayTeamTextView = itemView.findViewById(R.id.tv_away_team_name);
+            venueTextView = itemView.findViewById(R.id.tv_match_venue);
+            competitionTextView = itemView.findViewById(R.id.tv_competition);
         }
+
     }
 
 }
