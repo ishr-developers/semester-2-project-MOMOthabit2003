347a65d2f2ee7d0a8bdde9ded78016354f481c81
tony-albanese <tony.albanese2000@gmail.com>
2019-12-17 20:37:06 +0100

    Removed unused imports.

--- app/src/main/java/com/example/mohammedthabet/realmadrid/WebActivity.java
+++ app/src/main/java/com/example/mohammedthabet/realmadrid/WebActivity.java
@@ -1,14 +1,12 @@
 package com.example.mohammedthabet.realmadrid;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class WebActivity extends AppCompatActivity {
 
